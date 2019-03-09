89f18f6fc81724c2f6dee710eb451c625d8f10cf
mihailtoshev <mihail_13_99@yahoo.com>
2019-03-09 19:49:16 +0200

    Updates

---
+++ precache-manifest.70b53296d98073c7e741ac5326141866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "444c62a74db9b22c3edd",
+    "url": "/contactmanager/static/js/main.444c62a7.chunk.js"
+  },
+  {
+    "revision": "b6e2d7c25c8bd9ee9434",
+    "url": "/contactmanager/static/js/2.b6e2d7c2.chunk.js"
+  },
+  {
+    "revision": "444c62a74db9b22c3edd",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "b6e2d7c25c8bd9ee9434",
+    "url": "/contactmanager/static/css/2.f87932cc.chunk.css"
+  },
+  {
+    "revision": "ba5d28c05c5f931b123c32f3b2560072",
+    "url": "/contactmanager/index.html"
+  }
+];
